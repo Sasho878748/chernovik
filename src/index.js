82653dda09e1bcff91b8de947f11f9919cdbedc9
Sasho878748 <kovalenko878748@gmail.com>
2023-09-09 11:55:08 +0300

    q

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const arr = [1, 2, 3, [1, 2, 3], 4, 5, 6];
+
+// arr.forEach(element => {
+//     console.log(element);
+// });
+
+// const amout = arr.filter((item) => {
+//   return item < 5
+// });
+
+// console.log(amout);
+
+// const arrFlat = arr.flatMap(item =>  item);
+
+// console.log(arrFlat);
+
+// const sorty = arr.flatMap(i=>i).sort((a,b) => b - a);
+
+// console.log(sorty);
